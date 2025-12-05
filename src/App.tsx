e8413020a16adcbca2b4f275af926e70f89e2b07
kanjitta <jittakan2539@gmail.com>
2025-12-05 07:52:09 +0700

    initial commit && install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,7 @@ import './App.css';
 function App() {
 	return (
 		<>
-			<div>
-				<a href="https://www.digitalproductschool.io/" target="_blank">
-					<img src={dpsLogo} className="logo" alt="DPS logo" />
-				</a>
-			</div>
-			<div className="home-card">
-				<p>Your solution goes here 😊</p>
-			</div>
+			<p className="text-red-500">Let's start!</p>
 		</>
 	);
 }
