0297a2a5a425ac1f559de062fbb55594abfd7fd3
kanjitta <jittakan2539@gmail.com>
2025-12-07 14:08:39 +0700

    add states to keep town and postalCode values

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+
 function App() {
 
-	// Get PostalCode from name
+	const [town, setTown] = useState<string>("");
+	const [postalCode, setPostalCode] = useState<string | number>("");
+
+	// Get PostalCode from Town/City Name
 	const handleChangeTown = async (event: React.ChangeEvent<HTMLInputElement>) => {
 		const town = event.target.value;
 		const backendUrl= "https://openplzapi.org/de/"
@@ -19,7 +24,28 @@ function App() {
 		} catch (error) {
 			console.error('Error fetching data: ', error);
 		}
-	}		
+	}
+
+	// Get PostalCode from Town/City Name
+	const handleChangePostalCode = async (event: React.ChangeEvent<HTMLInputElement>) => {
+		const town = event.target.value;
+		const backendUrl= "https://openplzapi.org/de/"
+		try {
+			const url = backendUrl + 'Localities?name=' + town;
+			console.log('url', url);
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error(`Response status: ${response.status}`);
+			}
+
+			const result = await response.json();
+			console.log('result', result);
+			console.log('postal code', result[0].postalCode);
+
+		} catch (error) {
+			console.error('Error fetching data: ', error);
+		}
+	}	
 
 
 	return (
@@ -30,10 +56,20 @@ function App() {
 				{/* Town/City Input */}
 				<form action="" className="mt-10">
 				<label htmlFor="" className="text-3xl px-3">Town/City</label>
-				<input onChange={handleChangeTown} className="mt-2 px-2 bg-white border rounded-2xl " type="text" />
+				<input
+					value={town} 
+					onChange={handleChangeTown} 
+					className="mt-2 px-2 bg-white border rounded-2xl " 
+					type="text" 
+				/>
 
 				<label htmlFor="" className="text-3xl px-3">Zip Code</label>
-				<input className="mt-2 px-2 bg-white border rounded-2xl " type="text" />
+				<input
+					value={postalCode}
+					onChange={handleChangePostalCode}
+					className="mt-2 px-2 bg-white border rounded-2xl " 
+					type="text" 
+				/>
 				</form>
 
 			</article>
